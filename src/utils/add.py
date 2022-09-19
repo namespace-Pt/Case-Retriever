bad6127ba136f0fb9346f58357cef1d57873b9ed
namespace-Pt <namespace.pt@gmail.com>
2022-09-19 21:21:37 +0800

    1

--- src/utils/add.py
+++ src/utils/add.py
@@ -42,7 +42,14 @@ if __name__ == "__main__":
 
                     yield case
 
+        error_log_path = f"../../../Data/wenshu/{file}.failed.txt"
+        error_log_file = open(error_log_path, "a+")
 
         text_num = int(subprocess.check_output(["wc", "-l", file_path]).decode("utf-8").split()[0])
-        for x in tqdm(gendata(), desc="Indexing", total=text_num):
-            elastic.index(index="wenshu", document=x)
+        for i, x in enumerate(tqdm(gendata(), desc="Indexing", total=text_num, ncols=100)):
+            try:
+                elastic.index(index="wenshu", document=x, id=f"{file.split('.')[0]}_{i}")
+            except:
+                print("i")
+                error_log_file.write(json.dumps(x, ensure_ascii=False) + "\n")
+
